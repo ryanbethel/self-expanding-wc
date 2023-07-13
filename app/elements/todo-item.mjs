acb96a31926f8c4ba861d2c81772d080dc50510c
Ryan Bethel <ryan.bethel@gmail.com>
2023-07-13 10:10:40 -0400

    use seperate render

--- app/elements/todo-item.mjs
+++ app/elements/todo-item.mjs
@@ -17,6 +17,7 @@ ${sharedRender(state)}
     constructor() {
       super()
       this.expand = this.expand.bind(this)
+      this.render = this.render.bind(this)
     }
 
     connectedCallback(){
@@ -35,6 +36,7 @@ ${sharedRender(state)}
       }
     }
 
+
     expand({ force = false }={}){
       const selfExpand = this.getAttribute('self-expand')
       if (force || selfExpand!==null) {
@@ -48,11 +50,10 @@ ${sharedRender(state)}
         const store = {} // Todo: Connect client-side store
         const state = {attrs,store}
         const slotContent = this.innerHTML
-        ${sharedRender.toString()} 
         const fragment = document.createElement('div')
         fragment.replaceChildren(slotContent)
         fragment.attachShadow({mode: 'open'});
-        fragment.shadowRoot.innerHTML = sharedRender(state) 
+        fragment.shadowRoot.innerHTML = this.render(state) 
         const children = Array.from(fragment.childNodes)
         children.forEach(child => {
           const slot = child.assignedSlot
@@ -60,6 +61,18 @@ ${sharedRender(state)}
         })
         this.innerHTML = fragment.shadowRoot.innerHTML
       }
+    }
+
+    render(state) {
+      // return \${sharedRender(state)}()
+
+      // OR
+
+    return \`
+    <p>\${state.attrs?.priority || 'Normal'} Priority Todo: 
+      <slot></slot>
+    </p>
+    \`
     }
   }
 
